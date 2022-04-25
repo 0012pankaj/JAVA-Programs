a00aa24f52580a2728b0c2d83f1a46a75cc18c7e
0012pankaj <103897354+0012pankaj@users.noreply.github.com>
2022-04-26 03:11:27 +0530

    Update SquareRootOFNum.java

--- Number-Programs/SquareRootOFNum.java
+++ Number-Programs/SquareRootOFNum.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 
+/* Using Newton Raphon Method*/
 public class SquareRootOFNum {
-    
+    static void giveRoot(double num) {
+        double x = num; // absolute root(x)
+        for (int i = 0; i < 10; i++) {
+            x = (x + num / x) / 2;
+
+        }
+        System.out.println("Square Root of " + num + "=" + x);
+    }
+
     public static void main(String[] args) {
         System.out.print("enter num=");
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         double num = sc.nextDouble();
-       
-        for (double i =0.1; i<num; i+=0.1) {
-            if (i*i==num) {
-                System.out.println(i);
-                break;
-            }
-            
+        giveRoot(num);
+
     }
 }
-}
+
+
+
+/*enter num=4
+Square Root of 4.0=2.0
+enter num=2
+Square Root of 2.0=1.414213562373095*/
